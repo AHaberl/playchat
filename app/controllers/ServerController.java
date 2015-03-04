96b9f92e185571494d4c1d14b0413fb9a9772d96
AHaberl <andreashaberl@gmx.net>
2015-03-04 18:29:18 +0100

    add method skeleton for adding new server

--- app/controllers/ServerController.java
+++ app/controllers/ServerController.java
@@ -1,7 +1,17 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class ServerController extends Controller{
 
+	
+	public static Result addServer(){
+		
+		
+		
+		return ok("server added");
+	}
+	
+	
 }
