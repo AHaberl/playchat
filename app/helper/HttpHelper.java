4c324c4126ae44a6c4b648e9780a126bf727716c
AHaberl <andreashaberl@gmx.net>
2015-03-04 19:47:40 +0100

    fixed cross server communication

--- app/helper/HttpHelper.java
+++ app/helper/HttpHelper.java
@@ -1,13 +1,17 @@
 package helper;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import play.libs.Json;
 import models.Message;
 import models.Server;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import play.libs.Json;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -17,19 +21,19 @@ public class HttpHelper {
 	
 	public static void sendMessage(Server server,  Message message) throws IOException{
 		
-		String targetUrl = server.getName() + ":" + server.getPort();
-		URL url = new URL(targetUrl);
-		
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-		connection.setRequestMethod("POST");
-		connection.setRequestProperty("Content-Type", "application/json");
-		
-		DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+		String targetUrl = "http://" + server.getUrl() + ":" + server.getPort() + "/messages/new";
 		JsonNode json = Json.toJson(message);
+	
+		HttpClient http = new DefaultHttpClient();
+		HttpPost request = new HttpPost(targetUrl);
 		
-		out.writeBytes(json.toString());
-		out.flush();
-		out.close();
+		StringEntity params = new StringEntity(json.toString());
+		request.addHeader("content-type", "application/json");
+		request.setEntity(params);
+		
+		HttpResponse response = http.execute(request);
+		
+		http.getConnectionManager().shutdown();
 		
 		
 	}
