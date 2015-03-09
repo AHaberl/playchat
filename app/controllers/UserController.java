c4cae5bc1fbcb06497406f6b1dd2d71c2f5ff77f
bgoerzig <bgoerzig@gmail.com>
2015-03-09 09:17:58 +0100

    Rough implementation according to lecture

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -3,7 +3,6 @@ package controllers;
 import java.io.IOException;
 
 import models.Message;
-import models.Messages;
 import models.Server;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -15,15 +14,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class UserController extends Controller{	
-	
-	private static Messages messages = new Messages();
+
 	private static ObjectMapper mapper = new ObjectMapper();
 	
 	public static Result getMessages(){
 		
-		JsonNode json = Json.toJson(messages.getMessages());
+		//EMF hier so
+		//JsonNode json = Json.toJson(messages.getMessages());
 		
-		return ok(json);
+		return ok();
 	}
 
 	
@@ -32,7 +31,7 @@ public class UserController extends Controller{
 				
 				
 		Message message = mapper.readValue(json.toString(), Message.class);
-		messages.addMessage(message);
+		//messages.addMessage(message);
 		
 		for(Server server : ServerController.servers){
 			
