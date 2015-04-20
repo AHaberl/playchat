ca5618b00820703bbac6f873c6be706997d692c8
bgoerzig <bgoerzig@gmail.com>
2015-04-20 13:30:38 +0200

    First test user

--- app/helper/Global.java
+++ app/helper/Global.java
@@ -1,21 +1,34 @@
 package helper;
+import java.util.HashSet;
+import java.util.Set;
+
+import controllers.UserController;
+import models.User;
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
 
 public class Global extends GlobalSettings {
-//	/private static Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
-   // public static JedisCluster cluster;
+    public static JedisCluster cluster;
 	
     public void onStart(Application app) {
-//    	Logger.info("Application has started");
-//    	jedisClusterNodes.add(new HostAndPort("127.0.0.1", 6379));
-//    	JedisCluster cluster = new JedisCluster(jedisClusterNodes);
-//    	cluster.set("user", "Benny");    	
+    	Logger.info("Application has started");
+    	Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+    	jedisClusterNodes.add(new HostAndPort("127.0.0.1", 30001));
+    	JedisCluster cluster = new JedisCluster(jedisClusterNodes);
+    	User benny = new User();
+    	Long nextID = cluster.incr("userid");
+    	benny.setPassword("supersafe");
+    	benny.setUserName("Andi");
+    	benny.setStatus("");
+    	benny.setID(nextID.toString());
+    	cluster.set("users", benny.getID());
+    	cluster.hmset("user:"+benny.getID(), UserController.convertUser(benny));
     }
 
     public void onStop(Application app) {
-//    	String user = cluster.hget("users", "benny");
     	Logger.info("user");
     }
 
---
+++ app/models/Persistence.java
@@ -0,0 +1,5 @@
+package models;
+
+public interface Persistence {
+	public void persist();
+}
