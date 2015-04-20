9865bc82fdc5c6725abcb33ec387b7ec7a25fbd9
AHaberl <andreashaberl@gmx.net>
2015-04-20 12:17:20 +0200

    added user again

---
+++ app/models/User.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String userName;
+	private String status;
+	
+	@OneToMany(mappedBy = "owner", cascade = CascadeType.PERSIST)
+	private List<Message> messages = new ArrayList<Message>();
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt;
+}
